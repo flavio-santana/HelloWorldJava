9581eb396c9312435857cf334c35adc4905ab564
Flavio Santana <f.o.santana@gmail.com>
2021-11-17 08:04:10 -0300

    meu primeiro commit

---
+++ src/Hello.java
@@ -0,0 +1,39 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int valor1;
+		int valor2;
+		int soma; 
+		String texto;  
+		float valor3 = 2f/3f;
+		double valor4 = 9d/2d;
+		double valor5 = 2.8;
+		
+		
+		valor1 = 1; 
+		valor2 = 2; 
+		
+		soma = valor1 + valor2;
+		
+		texto = "Soma entre valores valor1 e valor2: ";
+		
+		System.out.println(valor1);
+		
+		System.out.println(valor2);
+		
+		System.out.println(valor3);
+		
+		System.out.println(valor4);
+		
+		System.out.println(valor5 + valor4);
+		
+		System.out.println(texto + soma);
+		
+		System.out.println("Soma entre valores valor4 e valor5: " + (valor4 + valor5));
+
+	}
+
+}
